fa4e0df2cbdb56df46346599282c8a9ff05e2f1f
AmitGupta7580 <amitgupta758000@gmail.com>
2022-03-06 15:21:42 +0530

    Update planner config

--- solution/src/solution.py
+++ solution/src/solution.py
@@ -7,7 +7,7 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from LocationDetector import LocationDetector
 import cv2
 
-def movebase_client(goal_x, goal_y, goal_ox, goal_oy, goal_oz):
+def movebase_client(goal_x, goal_y, goal_ox, goal_oy, goal_oz, goal_ow):
 
 	client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
 	client.wait_for_server()
@@ -20,6 +20,7 @@ def movebase_client(goal_x, goal_y, goal_ox, goal_oy, goal_oz):
 	goal.target_pose.pose.orientation.x = goal_ox
 	goal.target_pose.pose.orientation.y = goal_oy
 	goal.target_pose.pose.orientation.z = goal_oz
+	goal.target_pose.pose.orientation.w = goal_ow
 
 	client.send_goal(goal)
 	wait = client.wait_for_result()
@@ -37,7 +38,7 @@ if __name__ == "__main__":
 
 	try:
 		rospy.init_node('solution')
-		result = movebase_client(8.1, -5.9, 0.0, 0.7, 0.7)
+		result = movebase_client(8.1, -5.9, 0.0, 0.0, 0.7, 0.7)
 		if result:
 			rospy.loginfo("Goal execution done!")
 
