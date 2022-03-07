c9d011b9c7dd03ab65a31e761cbb2f2bdbb75b09
AmitGupta7580 <amitgupta758000@gmail.com>
2022-03-07 14:52:52 +0530

    Add subscriber with location_detector

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Technex'22 Labyrinth
 
 Solution node for Labyrinth event under Technex'22
+
+## Steps to run Autonomous navigation
+
+### Basic Steps
+
+1. Launch Gazebo
+2. Launch Rviz
+
+
+### Generating/saving map
+
+1. rosrun gmapping slam_gmapping scan:scan odam_frame:odom
+3. run teleop node
+2. rosrun map_server map_saver -f map_name
+
+
+### Autonomous navigation
+
+1. Publish aruco ( rostopic pub /aruco std_msgs/Int8 "data: 0" )
+2. run spy node ( rosrun spy spy.py )
+3. run solution node (roscd solution/src/ -> python3 solution.py )
--- solution/src/solution.py
+++ solution/src/solution.py
@@ -1,47 +1,69 @@
 #!/usr/bin/env python3
+import cv2
+import os
+import numpy as np
 
 import rospy
 import actionlib
+from coordi_msgs.msg import coordi
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Int8
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 from LocationDetector import LocationDetector
-import cv2
 
-def movebase_client(goal_x, goal_y, goal_ox, goal_oy, goal_oz, goal_ow):
+image = None
+orientation = None
 
-	client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-	client.wait_for_server()
 
-	goal = MoveBaseGoal()
-	goal.target_pose.header.frame_id = "husky_robot_model__base_link"
-	goal.target_pose.header.stamp = rospy.Time.now()
-	goal.target_pose.pose.position.x = goal_x
-	goal.target_pose.pose.position.y = goal_y
-	goal.target_pose.pose.orientation.x = goal_ox
-	goal.target_pose.pose.orientation.y = goal_oy
-	goal.target_pose.pose.orientation.z = goal_oz
-	goal.target_pose.pose.orientation.w = goal_ow
+def callback1(data):
+    global image, orientation
+    image = data.image
+    orientation = data.orientation
 
-	client.send_goal(goal)
-	wait = client.wait_for_result()
 
-	if not wait:
-		rospy.logger("Action server not available")
-		rospy.signal_shutdown("Action server not available")
-	else:
-		return client.get_result()
+def movebase_client(goal_x, goal_y, orientation):
 
-if __name__ == "__main__":
-	# image = cv2.imread("../../images/image1.png")
-	# lat, lon = LocationDetector().detect_location(image)
-	# print(lat, lon)
+    client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+    client.wait_for_server()
 
-	try:
-		rospy.init_node('solution')
-		result = movebase_client(8.1, -5.9, 0.0, 0.0, 0.7, 0.7)
-		if result:
-			rospy.loginfo("Goal execution done!")
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = "husky_robot_model__base_link"
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.pose.position.x = goal_x
+    goal.target_pose.pose.position.y = goal_y
+    goal.target_pose.pose.orientation.x = orientation[0]
+    goal.target_pose.pose.orientation.y = orientation[1]
+    goal.target_pose.pose.orientation.z = orientation[2]
+    goal.target_pose.pose.orientation.w = orientation[3]
 
-	except rospy.ROSInterruptException:
-		rospy.loginfo("Navigation test finished.")
+    client.send_goal(goal)
+    wait = client.wait_for_result()
 
+    if not wait:
+        rospy.logger("Action server not available")
+        rospy.signal_shutdown("Action server not available")
+    else:
+        return client.get_result()
+
+
+if __name__ == '__main__':
+    try:
+        rospy.Subscriber('/terrorist_location', coordi, callback1)
+        rospy.init_node('solution', anonymous=True)
+        rate = rospy.Rate(10)  # 10hz
+        while not rospy.is_shutdown():
+            if image and orientation:
+                orientation = list(orientation)
+                image = np.array(image)
+                image = image.reshape([379, 1049, 3])
+                image = image.astype(np.uint8)
+                lat, lon = LocationDetector().detect_location(image)
+                rospy.loginfo(f"New Goal Position : {lat}, {lon}, {orientation}")
+                result = movebase_client(lat, lon, orientation)
+                if result:
+                    rospy.loginfo("Goal execution done!")
+                break
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Navigation test finished.")
